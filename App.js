0b6694e4648f70b9021df48984c05b6a80573a65
somberSomni <kartunehustla@gmail.com>
2023-05-21 23:54:51 -0400

    use basic render

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+// import { ListItem, Text } from '@rneui/themed';
+
+const events = [
+  {
+    name: 'Event 1',
+    datetime: '2023-05-21T10:00:00',
+    user: 'John Doe',
+  },
+  {
+    name: 'Event 2',
+    datetime: '2023-05-22T14:30:00',
+    user: 'Jane Smith',
+  },
+  // Add more events as needed
+];
+
+// const renderItem = ({ item }) => (
+//   <ListItem bottomDivider>
+//     <ListItem.Content>
+//       <ListItem.Title>{item.name}</ListItem.Title>
+//       <ListItem.Subtitle>
+//         {item.datetime}
+//       </ListItem.Subtitle>
+//       <ListItem.Subtitle>{item.user}</ListItem.Subtitle>
+//     </ListItem.Content>
+//   </ListItem>
+// );
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h3>List of Events</Text>
+      <FlatList
+        data={events}
+        renderItem={() => null}
+        keyExtractor={(item) => item.name}
+      />
     </View>
   );
 }
@@ -13,8 +44,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
